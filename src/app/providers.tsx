5d8e405ca7e867100204a39cb9e965c2e7e11c13
SeoHee3478 <seohee3478@gmail.com>
2025-11-25 16:58:41 +0900

    [refactor] tanstack query 캐싱 전략 변경

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,8 +10,12 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60 * 1000, // 1분
-            refetchOnWindowFocus: false,
+            staleTime: 1000 * 60 * 60, // 1시간
+            gcTime: 1000 * 60 * 60 * 24, // 24시간(캐시 보관 시간)
+            refetchOnWindowFocus: false, // 탭 전환시 재요청 안함
+            refetchOnMount: false, // 컴포넌트 마운트시 재요청 안함
+            refetchOnReconnect: false, // 네트워크 재연결시 재요청안함
+            retry: 1,
           },
         },
       })
