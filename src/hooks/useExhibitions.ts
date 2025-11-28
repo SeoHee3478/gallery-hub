c87f05e6c3019e0c10df7520e884d7b3c583b0bd
SeoHee3478 <seohee3478@gmail.com>
2025-11-28 14:59:48 +0900

    [chore] 절대경로 변수 수정

--- src/hooks/useExhibitions.ts
+++ src/hooks/useExhibitions.ts
@@ -11,7 +11,7 @@ interface ExhibitionsResponse {
 async function fetchExhibitions(page: number): Promise<ExhibitionsResponse> {
   const baseUrl =
     typeof window === "undefined"
-      ? process.env.BASE_URL || "http://localhost:3000"
+      ? process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"
       : "";
 
   const response = await fetch(
