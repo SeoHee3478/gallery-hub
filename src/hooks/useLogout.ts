77cf0dc34018f81f7b565400955b4b14d4723cd9
SeoHee3478 <seohee3478@gmail.com>
2025-12-11 15:00:13 +0900

    [feat] 로그아웃시 useAuthStore의 logout 함수 실행, api 절대경로로 변경

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,12 +1,14 @@
+import { useAuthStore } from "@/store/useAuthStore";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 
 export function useLogout() {
   const router = useRouter();
+  const { logout: clearAuthState } = useAuthStore();
 
   return useMutation({
     mutationFn: async () => {
-      const response = await fetch("api/logout", {
+      const response = await fetch("/api/logout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -17,6 +19,7 @@ export function useLogout() {
       return response.json();
     },
     onSuccess: () => {
+      clearAuthState();
       router.push("/login");
     },
   });
