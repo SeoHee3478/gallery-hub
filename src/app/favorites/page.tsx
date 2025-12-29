217ba73307d9f8d190ab8c07921a823d44ea1d05
SeoHee3478 <seohee3478@gmail.com>
2025-12-29 21:32:25 +0900

    [fix] 변경한 좋아요 목록에 맞게 type 수정

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -2,13 +2,34 @@
 
 import FavoritedExhibitionsClient from "./components/FavoritedExhibitionsClient";
 
-export interface Exhibition {
-  id: string;
+export interface ExhibitionDetails {
+  seq: string;
   title: string;
-  venue: string;
   startDate: string;
   endDate: string;
-  imageUrl: string;
+  place: string;
+  realmName: string;
+  area: string;
+  sigungu: string;
+  price: string;
+  contents1: string;
+  url: string;
+  phone: string;
+  imgUrl: string;
+  gpsX: string;
+  gpsY: string;
+  placeUrl: string;
+  placeAddr: string;
+  placeSeq: string;
+}
+
+export interface WishlistItem {
+  id: number;
+  created_at: string;
+  user_id: string;
+  item_id: string;
+  item_type: string;
+  details: ExhibitionDetails;
 }
 
 export default function FavoritesPage() {
