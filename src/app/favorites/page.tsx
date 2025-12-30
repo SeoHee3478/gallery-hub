e0e0dd0fea9bccc49a8c53e01e11e8390d014063
SeoHee3478 <seohee3478@gmail.com>
2025-12-30 14:40:42 +0900

    [refactor] type 분리

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -2,36 +2,6 @@
 
 import FavoritedExhibitionsClient from "./components/FavoritedExhibitionsClient";
 
-export interface ExhibitionDetails {
-  seq: string;
-  title: string;
-  startDate: string;
-  endDate: string;
-  place: string;
-  realmName: string;
-  area: string;
-  sigungu: string;
-  price: string;
-  contents1: string;
-  url: string;
-  phone: string;
-  imgUrl: string;
-  gpsX: string;
-  gpsY: string;
-  placeUrl: string;
-  placeAddr: string;
-  placeSeq: string;
-}
-
-export interface WishlistItem {
-  id: number;
-  created_at: string;
-  user_id: string;
-  item_id: string;
-  item_type: string;
-  details: ExhibitionDetails;
-}
-
 export default function FavoritesPage() {
   return <FavoritedExhibitionsClient />;
 }
